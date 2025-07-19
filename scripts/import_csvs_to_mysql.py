514d092ce8ff3a2c4ec7dead646b061fa99eea90
Kim Melgar <91382709+ArkBluee@users.noreply.github.com>
2025-07-20 01:41:06 +0800

    import_csvs_to_mysql.py

--- scripts/import_csvs_to_mysql.py
+++ scripts/import_csvs_to_mysql.py
@@ -14,7 +14,7 @@ try:
             df.to_sql(table, con=engine, if_exists='replace', index=False, chunksize=10000)
             print(f"{table} imported successfully.")
 
-    print("✅ All CSV files imported successfully.")
+    print("All CSV files imported successfully.")
 
 except Exception as e:
     print("Error occurred:", e)
